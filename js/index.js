cb8b30940c2902a906e0dae4b390791160ec81ee
Hector <quimat2@hotmail.com>
2023-03-06 11:43:19 -0600

    avance 06/03/2023

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const cardTop = document.querySelector('#cardTop').content
 const contenido = document.querySelector('#contenido')
 const fragment = document.createDocumentFragment()
+const btnBuscar = document.getElementById("buscador")
 let topTwoHundred = []
 
 document.addEventListener('DOMContentLoaded', () =>
@@ -35,9 +36,32 @@ const creaCards = () =>
     {
         cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
         cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        
+        let artists = ''
+        let size = song.trackMetadata.artists.length
+        song.trackMetadata.artists.forEach((item, index) =>
+        {
+            //console.log(index, size)
+
+            if (index === size - 1)
+            {
+                artists += item.name
+            } 
+            
+            else
+            {
+                artists += item.name + ' / '
+            }
+        })
+        cardTop.querySelector('.artistname').textContent = artists
 
         const clone = cardTop.cloneNode(true)
         fragment.appendChild(clone)
     })
     contenido.appendChild(fragment)
 }
+
+btnBuscar.addEventListener('keypress', () =>
+{
+    console.log('tecla', btnBuscar.value)
+})
